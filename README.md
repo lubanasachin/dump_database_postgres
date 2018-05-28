87e7acd4683ba4dee21c50cf374bf8f86ed3ef2f
sachin.lubana <sachin.lubana@b2x.com>
2018-05-28 09:04:55 +0100

    database dump : schema and data for dev|uat|prod environment

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# dump_database_postgres
+INTRODUCTION
+------------
+ 
+Shell script to dump PostgreSql database
+
+
+INSTALLATION/ TEST/ EXECUTION
+-----------------------------
+	
+ * ./dump_database.sh [env] [schema]
+	-> env defines the environment [dev|uat|prod]
+	-> schema defines whether only schema or data
+
+ 
+MAINTAINERS
+-----------
+
+Current maintainers:
+ * Sachin Lubana 
+ * Blog: http://sachinlubana.wordpress.com
+ * Skype: lubanasachin70
+ * Twitter: @lubanasachin70
+ * Linkedin: https://www.linkedin.com/in/sachinlubana
+
---
+++ dump_database.sh
@@ -0,0 +1,76 @@
+#set environment argument
+envP=$1
+
+#set schema only
+schema=$2
+
+#check if environment variable not empty
+if [ -z "$envP" -a "$envP" == "" ] 
+then
+	printf "\nUSAGE: ./dump_database.sh [dev|uat|prod] [schema:yes|no]\n\n"
+	exit
+fi
+
+#set db configs based on given environment
+if [ $envP == 'dev' ]
+then
+	hName='192.168.x.y'
+	hUser='user'
+	hPass='pass'
+	dbList=( db1 db2 )
+elif [ $envP == 'uat' ]
+then
+	hName='db1-xyz.rds.amazonaws.com'
+	hUser='user'
+	hPass='pass'
+	dbList=( db1 db2 )
+elif [ $envP == 'prod' ]
+then
+	hName='db2-xyz.rds.amazonaws.com'
+	hUser='user'
+	hPass='pass'
+	dbList=( db1 db2 )
+else 
+	printf "\nUSAGE: ./dump_database.sh [dev|uat|prod] [schema:yes|no]\n\n"
+	exit
+fi
+
+printf "\nSTARTED \n\n"
+
+set PGPASSWORD=$hPass
+
+#create backup directory if not exists
+backupDir='BACKUP/DATABASE/'
+mkdir -p $backupDir 
+cd $backupDir
+
+#loop over db's configured
+for i in "${dbList[@]}"
+do
+:
+	echo "Running for "$i
+
+	#create env and db folder 
+	mkdir -p $envP/$i
+
+	#change directory 
+	cd $envP/$i
+
+	#check if schema only dump required
+	if [ ! -z "$schema" -a "$schema" != "" ] 
+	then
+		echo /usr/bin/pg_dump -v -s -U "'$hUser'" -p 5432 -h "'$hName'" -d "'$i'" -f dump_$i.sql
+		/usr/bin/pg_dump -v -s -U "$hUser" -p 5432 -h "$hName" -d "$i" -f dump_$i.sql
+	else
+		echo /usr/bin/pg_dump -v -U "'$hUser'" -p 5432 -h "'$hName'" -d "'$i'" -f dump_$i.sql
+		/usr/bin/pg_dump -v -U "$hUser" -p 5432 -h "$hName" -d "$i" -f dump_$i.sql
+	fi
+
+	printf "Completed for $i\n\n"
+
+	#move back to main dir
+	cd ../../
+
+done
+
+printf "\nCOMPLETED \n\n"
